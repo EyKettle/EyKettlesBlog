bc9f8451faca702854102b7ef943b1598deb5f9a
EyKettle <EyKettle@163.com>
2025-02-15 20:52:16 +0800

    fix(deno): 修复入口文件路径问题

--- public/main-250214.ts
+++ public/main-250214.ts
@@ -1,25 +1,31 @@
 import { serveFile } from "https://deno.land/std@0.217.0/http/file_server.ts";
 import * as path from "https://deno.land/std@0.217.0/path/mod.ts";
 
-export const STATIC_EXTENSIONS = new Set(["zip", "rar", "webp"]);
+export const STATIC_EXTENSIONS = new Set([
+  "ico",
+  "js",
+  "css",
+  "ttf",
+  "zip",
+  "rar",
+  "webp",
+]);
 
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
   const pathname = url.pathname;
 
   const fileExtension = pathname.split(".").pop()?.toLowerCase();
-  const parentDir = path.dirname(Deno.cwd());
-
   if (fileExtension && STATIC_EXTENSIONS.has(fileExtension)) {
     try {
-      return await serveFile(req, path.join(parentDir, "dist", pathname));
+      return await serveFile(req, path.join(Deno.cwd(), pathname));
     } catch (error) {
       console.error("Error serving static file: ", error);
       return new Response("No such file", { status: 404 });
     }
   } else {
     try {
-      return await serveFile(req, path.join(parentDir, "dist", "index.html"));
+      return await serveFile(req, path.join(Deno.cwd(), "index.html"));
     } catch (error) {
       console.error("Error serving index.html: ", error);
       return new Response("Website disappeared", { status: 404 });
